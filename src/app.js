a0fd2292b857b90e572d194071480dc4b4136d98
Nihalxyztr <tripathinihal2006@gmail.com>
2026-02-21 00:54:30 +0530

    feature: Request Tracing with Mongo DB implemented

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import gatewayRoutes from "./routes/gateway.routes.js";
-
+import tracingMiddleware from "./middleware/trace.middleware.js";
 
 const app=express();
 app.use(express.json());
+app.use(tracingMiddleware);
 
 app.use("/", gatewayRoutes);
 
