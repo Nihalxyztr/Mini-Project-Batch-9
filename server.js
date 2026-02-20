9911da9a13c8ec4e83b39beba8bba0f8654bd3ed
Nihalxyztr <tripathinihal2006@gmail.com>
2026-02-20 21:02:55 +0530

    Core:Added Mongo Db Connection

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 const PORT=5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})  ;
+const startServer=async()=>{
+    try{
+        await connectDB();
+        app.listen(PORT,()=>{
+            console.log(`Server is running on port ${PORT}`);
+            
+        });
+    }
+    catch(err){
+        console.log("error starting the server",err)
+    }
+};
+
+startServer();
