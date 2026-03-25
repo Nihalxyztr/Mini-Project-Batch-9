865b0cd7d074bfb483f5c2e3338354806f7aa9d7
Nishant Arun <nishantarun2512@gmail.com>
2026-03-25 10:31:50 +0530

    feat(user): add user controller with create and fetch endpoints

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import gatewayRoutes from "./routes/gateway.routes.js";
 import tracingMiddleware from "./middleware/trace.middleware.js";
+import userRoutes from "./routes/user.routes.js";
 
-const app=express();
+const app = express();
 app.use(express.json());
 app.use(tracingMiddleware);
 
 app.use("/", gatewayRoutes);
+app.use("/api", userRoutes);
 
 export default app;
