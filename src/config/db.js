f2f1231040fce8e00868618f0d536460854aa0e1
Omsharma2807 <oms605027@gmail.com>
2026-04-15 06:49:45 +0530

    Reviewed repo and added changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
  
 export async function connectDB() {
   await mongoose.connect(process.env.MONGO_URI);
-  console.log("Connected to MongoDB")
+  console.log("MongoDB connected");
 }
